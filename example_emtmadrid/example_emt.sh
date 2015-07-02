6d9e641e226ed558554376a1973391b9c879b3ab
Enrique Otero <enrique.otero@beeva.com>
2015-07-02 12:09:31 +0200

    EMT example script

--- example_emtmadrid/example_emt.sh
+++ example_emtmadrid/example_emt.sh
@@ -1,4 +1,6 @@
 # OPENDATA EMT
+
+### ETL ###
 # GetRouteLines gets information about routes, distances and coordinates
 curl -k --data "idClient=$EMT_ID_CLIENT&passKey=$EMT_PASS_KEY" https://openbus.emtmadrid.es:9443/emt-proxy-server/last/bus/GetRouteLines.php > data/EMTRouteLines.json
 
@@ -14,6 +16,18 @@ echo -e "line,secDetail,orderDetail,node,distance,distancePreviousStop,name,lati
 # With python and pandas
 cat data/EMTRouteLines.json | python ./EMTRouteLines_to_csv.py > testdata/EMTRouteLines.csv
 
-# Start ipython notebook. Load and describe data. Plot histograms
 
+
+### Python notebook ###
+# Start ipython notebook. Load and describe data. Plot histograms
+# GetListLines gets information about lines
 curl -k --data "idClient=$EMT_ID_CLIENT&passKey=$EMT_PASS_KEY&SelectDate=01/07/2015" https://openbus.emtmadrid.es:9443/emt-proxy-server/last/bus/GetListLines.php > data/EMTListLines.json
+
+
+
+### CartoDB ###
+# Load RouteLines
+# View Map, simple, heatmap
+# SQL queries
+# PostGIS queries
+# wizard CSS and infowindow
