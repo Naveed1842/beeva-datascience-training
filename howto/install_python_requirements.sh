a07d29ab6839d19f150494e2022903dc7c2fd9ad
Enrique Otero <enrique.otero@beeva.com>
2015-07-03 15:12:38 +0200

    Fix install_python_requirements.sh

--- howto/install_python_requirements.sh
+++ howto/install_python_requirements.sh
@@ -1,15 +1,19 @@
 # Recomendación:
-# instalar ipython notebook, pandas y matplotlib usando un entorno virtual
+# instalar ipython notebook, pandas, matplotlib, scikit-learn y scipy usando un entorno virtual
 # This worked for me ;)
-cd $WHATEVER_YOU_WANT
-mkdir datascience-training
+cd ~ #or $WHATEVER_YOU_WANT
+#sudo apt-get install python-pip
+#sudo pip install virtualenv
 virtualenv datascience-training
-source datascience-traning
+source datascience-training/bin/activate
 pip install ipython["notebook"]
 pip install functools32
 pip install pandas
 pip install -U distribute
 pip install matplotlib
+pip install requests
+pip install scikit-learn
+pip install scipy
 
 # Opción alternativa:
 # instalar la distribución Anaconda para python científico: http://docs.continuum.io/anaconda/install.html#anaconda-install
